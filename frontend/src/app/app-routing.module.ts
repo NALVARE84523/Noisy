78adb4a8d154f67e85b6fdaeff74e7c13958e6b6
Nelson Alvarez <nalvare84523@universidadean.edu.co>
2020-11-13 13:56:27 -0500

    Rutas de login y registro

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { SignupRegisterComponent } from './signup/signup-register/signup-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginPageComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'registro',
+    component: SignupRegisterComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
